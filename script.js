223c40a70981cfa369f213a973b54ffba49ebcc4
Abdulmalik <103919618+0300hrs@users.noreply.github.com>
2024-01-09 21:29:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const adviceNumberElement = document.getElementById("adv-num");
   const adviceTextElement = document.getElementById("gadvice");
 
-  // Function to fetch advice from the https://api.adviceslip.com/advice endpoint
   const fetchAdvice = async () => {
     try {
       const response = await fetch("https://api.adviceslip.com/advice");
       const data = await response.json();
 
-      // Extracting slip_id and advice from the API response
+
       const { id, advice } = data.slip;
 
-      // Update HTML elements with fetched data
       adviceNumberElement.textContent = id;
       adviceTextElement.textContent = advice;
       console.log(data.slip);
@@ -23,9 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  // Add click event listener to the dice element
+
   diceElement.addEventListener("click", fetchAdvice);
 
-  // Initial advice fetch when the page loads
   fetchAdvice();
 });
